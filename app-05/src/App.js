b3338bc5e44553202e3898c1bb89a8d5d00b9451
mithon42 <mithonphilip@gmail.com>
2022-08-14 05:42:44 +0545

    Added New Folder

--- app-05/src/App.js
+++ app-05/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {socialIcons} from './data'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Apps</h1>
+
+      <div>
+        {socialIcons.map(item => (
+          <div key={item.id}>
+            <FontAwesomeIcon icon={['fab', item.icon]}/>
+          </div>
+          ))}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ app-05/src/data.js
@@ -0,0 +1,19 @@
+
+
+
+export const socialIcons = [
+  {
+    id: '123',
+    icon: 'facebook',
+    color: '#3A5890',
+    tooltip: 'share on facebook',
+    link: 'https://fb.com'
+  },
+  {
+    id: '134',
+    icon: 'twitter',
+    color: '#3A5890',
+    tooltip: 'share on twitter',
+    link: 'https://twitter.com'
+  }
+];
